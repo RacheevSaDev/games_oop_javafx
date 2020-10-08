c0bc3d75d2f4bbba2878b900d20dcb5842e0ff1f
racheev <racheev.sa.dev@gmail.com>
2020-10-09 00:43:42 +0700

    2. Сокобан. Тестовое задание. [#371000]
    Доработан класс Win,
    Исправлен не корректный WinTest.whenNotWin,
    Исправлены мелкие ошибки в тесте

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,34 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i=0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                rsl = isMonoHorizontal(board, i, 1) || isMonoVertical(board, i, 1);
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean isMonoHorizontal(int[][] board, int index, int value) {
         boolean rsl = true;
+        for (int j = 0; j < board.length; j++) {
+            if (board[index][j] != value) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean isMonoVertical(int[][] board, int index, int value) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][index] != value) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
